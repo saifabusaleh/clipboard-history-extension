17a718e30a1dea4c82b6f7ddbe86dd42894bf3fb
Saif <saif.as7777@gmail.com>
2020-06-27 14:50:08 +0300

    update styles

--- src/browser_actions/popup.js
+++ src/browser_actions/popup.js
@@ -3,7 +3,13 @@ window.onload = function () {
         code: "window.getSelection().toString();"
     }, function (selection) {
         chrome.runtime.sendMessage({ selection: selection[0] }, function (response) {
-            document.getElementById("output").innerHTML = response.clips;
+            // document.getElementById("output").innerHTML = response.clips;
+            const ul = document.getElementById("outputList");
+            response.clips && response.clips.forEach((clip) => {
+                const li = document.createElement("li");
+                li.appendChild(document.createTextNode(clip));
+                ul.appendChild(li);
+            });
         });
     });
 
