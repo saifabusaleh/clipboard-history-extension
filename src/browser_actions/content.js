25579d87e20e98a2cf0672ce8604746b8edfcfbd
Saif <saif.as7777@gmail.com>
2020-07-04 22:15:29 +0300

    save text only when finishing selection

--- src/browser_actions/content.js
+++ src/browser_actions/content.js
@@ -1,6 +1,6 @@
-document.addEventListener('selectionchange', () => {
+window.addEventListener("mouseup", function (event) {
     console.log(window.getSelection().toString());
-    chrome.runtime.sendMessage({ selection: window.getSelection().toString() }, (response) =>  {
+    chrome.runtime.sendMessage({ selection: window.getSelection().toString() }, (response) => {
         //
     });
 });
