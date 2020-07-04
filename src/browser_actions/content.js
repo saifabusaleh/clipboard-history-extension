519e599faed745e3b0136e63a4f1a9eedbd30bd1
Saif <saif.as7777@gmail.com>
2020-07-04 22:17:19 +0300

    save text only when selection is not empty

--- src/browser_actions/content.js
+++ src/browser_actions/content.js
@@ -1,6 +1,9 @@
 window.addEventListener("mouseup", function (event) {
-    console.log(window.getSelection().toString());
-    chrome.runtime.sendMessage({ selection: window.getSelection().toString() }, (response) => {
-        //
-    });
+    const text = window.getSelection().toString();
+    if(text && text.length > 0) {
+        console.log(window.getSelection().toString());
+        chrome.runtime.sendMessage({ selection: window.getSelection().toString() }, (response) => {
+            //
+        });
+    }
 });
