139a0f8d7557cfedcdbbac3dd4149f396dc77df7
Saif <saif.as7777@gmail.com>
2020-07-07 19:53:54 +0300

    refactor

--- src/browser_actions/content.js
+++ src/browser_actions/content.js
@@ -2,8 +2,6 @@ window.addEventListener("copy", function (event) {
     const text = window.getSelection().toString();
     if(text && text.length > 0) {
         // console.log(window.getSelection().toString());
-        chrome.runtime.sendMessage({ selection: window.getSelection().toString() }, (response) => {
-            //
-        });
+        chrome.runtime.sendMessage({ selection: window.getSelection().toString() });
     }
 });
