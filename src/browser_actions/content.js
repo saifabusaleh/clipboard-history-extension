4ed7d81cc6debfb7a311e582c2b67ed91b1c6804
Saif <saif.as7777@gmail.com>
2020-07-12 20:28:53 +0300

    fix input/textarea copying is not working

--- src/browser_actions/content.js
+++ src/browser_actions/content.js
@@ -1,6 +1,6 @@
 window.addEventListener("copy", function (event) {
     try {
-        const text = window.getSelection().toString().trim();
+        const text = getSelectionText();
         if (text && text.length > 0) {
             chrome.runtime.sendMessage({ selection: window.getSelection().toString() });
         }
@@ -8,3 +8,19 @@ window.addEventListener("copy", function (event) {
         console.error(e);
     }
 });
+
+function getSelectionText() {
+    var text = "";
+    var activeEl = document.activeElement;
+    var activeElTagName = activeEl ? activeEl.tagName.toLowerCase() : null;
+    if (
+        (activeElTagName == "textarea") || (activeElTagName == "input" &&
+            /^(?:text|search|password|tel|url)$/i.test(activeEl.type)) &&
+        (typeof activeEl.selectionStart == "number")
+    ) {
+        text = activeEl.value.slice(activeEl.selectionStart, activeEl.selectionEnd);
+    } else if (window.getSelection) {
+        text = window.getSelection().toString();
+    }
+    return text;
+}
